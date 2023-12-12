cfce86493361824286d5a856f54d926f8bad5f8f
Nassim <118028140+Nassim-nt@users.noreply.github.com>
2023-12-12 14:15:15 -0600

    Update streamlit_app.py
    added pictures and one menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & blueberry Oatmeal")
-streamlit.text("Kale, Spinash & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Omega 3 & blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinash & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")    
+streamlit.text("🥑🍞 Avocado Toast")
 
 
 
