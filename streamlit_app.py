adf709df7828ba70c0584ca6cf30efe10a08b835
Nassim <118028140+Nassim-nt@users.noreply.github.com>
2023-12-12 14:11:46 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.test("Omega 3 & blueberry Oatmeal")
+streamlit.text("Omega 3 & blueberry Oatmeal")
 streamlit.text("Kale, Spinash & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
 
